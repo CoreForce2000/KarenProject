2b2e3a7d4415e3b84e797ebb60e44e05418047e4
Maxime MacNiven <maxime.macniven@sap.com>
2023-08-21 12:18:31 +0200

    Currently working on shopping cart

---
+++ app/start.js
@@ -0,0 +1,64 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // // view engine setup
+// // app.set('views', path.join(__dirname, 'views'));
+// // app.set('view engine', 'jade');
+
+// // This route serves your index.html file
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'app', 'index.html'));
+// });
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// This tells Express to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// This route serves your index.html file
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = 8080;
+app.listen(port, function() {
+    console.log(`Server listening on port ${port}`);
+});
---
+++ stuff/app/push.bat
@@ -0,0 +1,6 @@
+
+cf8.exe login -a https://api.cf.sap.hana.ondemand.com -o "PSApps: Test"
+
+cf8.exe push
+
+PAUSE
